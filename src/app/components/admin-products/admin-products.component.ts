3acf7c90c2cfb439d9fad947064dd3211ebe5460
cbenz <contactocbenitez@gmail.com>
2023-10-13 10:36:28 -0300

    fix prdouct form

--- src/app/components/admin-products/admin-products.component.ts
+++ src/app/components/admin-products/admin-products.component.ts
@@ -2,36 +2,47 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from 'src/app/models/product';
 
-import { MatTableModule } from "@angular/material/table"
+import { MatTableModule } from '@angular/material/table';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { AddBookButtonComponent } from '../add-book-button/add-book-button.component';
 import { ProductFormComponent } from '../product-form/product-form.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-admin-products',
   standalone: true,
-  imports: [CommonModule, MatTableModule, AddBookButtonComponent, MatDialogModule, ProductFormComponent],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    AddBookButtonComponent,
+    MatDialogModule,
+    ProductFormComponent,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
   templateUrl: './admin-products.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AdminProductsComponent {
   private _dialog: MatDialog = inject(MatDialog);
-  public columns: Array<string> = ["title", "stock", "price", "visibility"];
+  public columns: Array<string> = ['title', 'stock', 'price', 'visibility'];
 
-  public products: Array<Product> = [{
-    title: "Yo, Robot",
-    authors: ["Isaac Asimov"],
-    cover: "https://www.penguinlibros.com/uy/1651349-thickbox_default/yo-robot.jpg",
-    summary: "un librito",
-    price: 650.00,
-    stock: 20
-  }];
-  
+  public products: Array<Product> = [
+    {
+      title: 'Yo, Robot',
+      authors: ['Isaac Asimov'],
+      cover:
+        'https://www.penguinlibros.com/uy/1651349-thickbox_default/yo-robot.jpg',
+      summary: 'un librito',
+      price: 650.0,
+      stock: 20,
+    },
+  ];
 
   edit(product: Product) {
     this._dialog.open(ProductFormComponent, {
-      data: product
-    })
+      data: product,
+    });
   }
 }
--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,15 +1,13 @@
-import { Component, Inject, Input, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule, FormBuilder, Validators } from "@angular/forms";
+
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog"
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import {
-  MatCheckboxChange,
-  MatCheckboxModule,
-} from '@angular/material/checkbox';
-import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from "@angular/material/datepicker" 
 import { Product } from 'src/app/models/product';
 
 @Component({
@@ -19,31 +17,46 @@ import { Product } from 'src/app/models/product';
     CommonModule,
     ReactiveFormsModule,
     MatInputModule,
-    MatDatepickerModule,
+    MatFormFieldModule,
     MatButtonModule,
-    MatCheckboxModule,
-    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   templateUrl: './product-form.component.html',
-  styles: [],
+  styles: [
+  ]
 })
 export class ProductFormComponent {
-  @Inject(MAT_DIALOG_DATA) product!: Product
-  private _dialogRef: MatDialogRef<ProductFormComponent> = inject(
-    MatDialogRef<ProductFormComponent>
-  );
-  private _formBuilder: FormBuilder = inject(FormBuilder);
-  discount = false;
-  public productForm = this._formBuilder.group({
-    title: [this.product.title ?? ''],
-    description: [this.product.title ?? ''],
-    stock: [this.product.title ?? 0],
-    price: [this.product.title ?? 0],
-    visibility: [this.product.title ?? 'enabled']
+  private _dialogRef: MatDialogRef<ProductFormComponent> = inject(MatDialogRef<ProductFormComponent>);
+  private _data: Product = inject(MAT_DIALOG_DATA);
+  private _formBuider: FormBuilder = inject(FormBuilder);
+  public productForm = this._formBuider.group({
+    title: [this._data?.title || '', Validators.required],
+    price: [this._data?.price || 0, Validators.required],
+    summary: [this._data?.summary || '', Validators.required],
+    authors: [this._data?.authors || '', Validators.required],
+    cover: [this._data?.cover || ''],
+    stock: [this._data?.stock || 0, Validators.required]
   });
 
-  onDiscountCheckbox(event: MatCheckboxChange) {
-    if (event.checked) this.discount = true;
-    else this.discount = false;
+  public discount: boolean = false;
+
+  coverChange(evt: any) {
+    
+  }
+
+  discountChange(evt: any) {
+    this.discount = evt.target.checked;
+  }
+
+  upload() {
+    console.log(this.productForm.value)
+    this._dialogRef.close()
+
+    
+  }
+
+  now() {
+    return new Date().toISOString().split("T")[0]
   }
 }
